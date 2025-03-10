0aa8cd0d379d3918b841d1a9f37dddaf3f5054de
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2025-03-10 21:22:44 +0100

    feat: spread and find utilities

--- internal/utils/fitler.go
+++ internal/utils/fitler.go
@@ -1,5 +1,7 @@
 package utils
 
+import "reflect"
+
 func Filter[T any](vs []T, pred func(t T, ind int) bool) []T {
 	var result []T
 
@@ -10,4 +12,44 @@ func Filter[T any](vs []T, pred func(t T, ind int) bool) []T {
 	}
 
 	return result
+}
+
+func Find[T any](vs []T, pred func(t T, ind int) bool) *T {
+
+	var t *T
+	for i, val := range vs {
+		if pred(val, i) {
+			t = &vs[i]
+		}
+	}
+	return t
+}
+
+/*
+Spread takes to generic structs and returns a "merged" struct where `new` is spread into `old`,
+```
+	return {...old, ...new} //javscript like
+```
+*/
+func Spread[T any](old, new T) T {
+
+	out := old
+
+	oldVal := reflect.ValueOf(&out).Elem()
+	newVal := reflect.ValueOf(new)
+	if newVal.Kind() == reflect.Ptr {
+		newVal = newVal.Elem() // Dereference if new is a pointer
+	}
+
+	for i := 0; i < newVal.NumField(); i++ {
+		field := newVal.Type().Field(i)
+		srcField := newVal.Field(i)
+
+		if !srcField.IsZero() {
+			oldVal.FieldByName(field.Name).Set(srcField)
+		}
+	}
+
+	return out
+
 }
