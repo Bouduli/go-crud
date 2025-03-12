9c16880704e634063ff903aaf97a21afb3ce023a
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2025-03-12 21:41:28 +0100

    feat: db.Start function

--- internal/db/database.go
+++ internal/db/database.go
@@ -2,5 +2,35 @@ package db
 
 import (
 	"database/sql"
+	"os"
+	"path"
+
 	_ "github.com/mattn/go-sqlite3"
 )
+
+var DB *sql.DB = nil
+
+//TODO: figure out some way to handle this "better"
+
+// Starts the SQLite database and returns it
+func Start() (*sql.DB, error) {
+	appRoot, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := sql.Open("sqlite3", path.Join(appRoot, "db", "app.db"))
+	if err != nil {
+		return nil, err
+	}
+
+	DB = db
+
+	return DB, nil
+}
+
+// Close
+// closes the database
+func Close() error {
+	return DB.Close()
+}
